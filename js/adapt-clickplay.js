5602899c89e5e3089a3248c4c360d8ee27592d5d
daya-cd <h.dayanithi@gmail.com>
2016-08-25 15:22:10 +0100

    intial setup for click and play plugin

---
+++ js/adapt-clickplay.js
@@ -0,0 +1,43 @@
+define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adapt) {
+
+	var clickPlay = ComponentView.extend({
+
+
+		preRender: function() {
+			
+
+             // Checks to see if the blank should be reset on revisit
+             this.checkIfResetOnRevisit();
+
+
+
+         },
+         postRender: function() {
+         	this.setReadyStatus();
+         	this.$('.component-inner').on('inview', _.bind(this.inview, this));
+            
+         },
+
+        // Used to check if the blank should reset on revisit
+        checkIfResetOnRevisit: function() {
+        	var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+
+            // If reset is enabled set defaults
+            if (isResetOnRevisit) {
+            	this.model.reset(isResetOnRevisit);
+            }
+        },
+
+        inview: function(event, visible, visiblePartX, visiblePartY) {
+            // console.log(event);
+            // $('#footer').append("<div id='reading-background'><img src='assets/progress.png'></div>");
+     }
+
+
+
+ });
+	Adapt.register('clickplay', clickPlay);
+
+	return clickPlay;
+
+});
